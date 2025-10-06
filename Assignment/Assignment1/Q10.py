3e6b112da1e1d81426951beb4b25d7b9242c1fd2
Saniya Pradipkumar Tembare <saniyatembare73@gmail.com>
2025-10-06 22:12:06 +0530

    Delete Assignment directory

--- Assignment/Assignment1/Q10.py
+++
@@ -1,7 +0,0 @@
-#Calculate area of the equilateral triangle
-a = float(input('Enter side of equiletral triangle : '))
-
-#Perform operation
-area = (3 ** 0.5 / 4) * (a ** 2)
-
-print(f'Area of equaliteral trinagle is {area}')
--- Assignment/Assignment1/Q2.py
+++
@@ -1,9 +0,0 @@
-#Take length and breadth and calculate area of rectangle
-length = float(input('Enter the lenght of triangle : '))
-breadth = float(input('Enter the breadth of the rectangle : '))
-
-#perform operation
-Area = length * breadth
-
-#display result
-print(f'Area of reactangle is {Area}.')
