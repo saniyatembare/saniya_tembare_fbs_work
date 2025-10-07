e6f26a86923a3ff24399b4be6d38bd2a6d758418
Saniya Pradipkumar Tembare <saniyatembare73@gmail.com>
2025-10-07 12:46:37 +0530

    Update Q2.py

--- fbs/CorePython/Test/Test1/Q2.py
+++ fbs/CorePython/Test/Test1/Q2.py
@@ -1,9 +1,8 @@
-# Calculate simpel interest 
+# Calculate simple interest 
 p = int(input('Enter the principle amount : '))
 r = int(input('Enter the rate : '))
 t = int(input('Enter the time : '))
 
 SI = (p * r * t) / 100
-
 
 print(f'Simple interest of the amount is {SI}')
