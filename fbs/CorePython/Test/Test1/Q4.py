5e6bee6d9b40a95ac7b13e10f836af383885ca9a
Saniya Pradipkumar Tembare <saniyatembare73@gmail.com>
2025-10-09 20:59:32 +0530

    Update Q4.py

--- fbs/CorePython/Test/Test1/Q4.py
+++ fbs/CorePython/Test/Test1/Q4.py
@@ -5,11 +5,12 @@ area = int(input('Enter the area of one wall : '))
 inner_cost = int(input('Enter the interior cost : '))
 outer_cost = int(input('Enter the exterior cost : '))
 
-inner_wall = 7
-outer_wall = 6
+inner_wall = 8
+outer_wall = 7
 
 Total_inner = inner_wall * area * inner_cost
 Total_outer = outer_wall * area * outer_cost
 Total = Total_inner + Total_outer
+
 
 print(f'Interior wall cost is {Total_inner}, and Exterior wall cost is {Total_outer}, and the Total cost is {Total}')
