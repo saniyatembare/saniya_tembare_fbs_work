204308d545bedb500ae3debcf7cf497858ccb163
Saniya Pradipkumar Tembare <saniyatembare73@gmail.com>
2025-11-09 12:00:16 +0530

    Delete fbs/CorePython directory

--- fbs/CorePython/Test/Test1/Q2.py
+++
@@ -1,8 +0,0 @@
-# Calculate simple interest 
-p = int(input('Enter the principle amount : '))
-r = int(input('Enter the rate : '))
-t = int(input('Enter the time : '))
-
-SI = (p * r * t) / 100
-
-print(f'Simple interest of the amount is {SI}')
--- fbs/CorePython/Test/Test1/Q3.py
+++
@@ -1,7 +0,0 @@
-dis_km = int(input('Enter the distance in km : '))
-
-meter = dis_km * 1000
-
-centimeter = dis_km * 1000000
-
-print(f'{dis_km} km is {meter} m and {centimeter} cm')
