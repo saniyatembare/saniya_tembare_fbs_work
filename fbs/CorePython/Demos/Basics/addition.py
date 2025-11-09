598d6794b9c0261fd36fb214e18f8dcc9cf62a88
Saniya Pradipkumar Tembare <saniyatembare73@gmail.com>
2025-11-09 12:00:05 +0530

    Delete fbs/CorePython/Demos directory

--- fbs/CorePython/Demos/Basics/addition.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input('Enter number1 : '))
-num2 = int(input('Enter number2 : '))
-sum = num1 + num2
-print(sum)
-print('Addition is',sum)
-print('Addition of', num1, '&', num2, 'is', sum, '.')
-print('addition of '+str(num1)+' & ' +str(num2) +' is '+str(sum)+'.')
-print(f'Addition of {num1} and {num2} is {sum}.')
